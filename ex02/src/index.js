6ca258e955cc030a12392fa20197a461a9691328
Haris Pasic <harispasic@ymail.com>
2022-03-30 21:47:29 +0200

    Create ex02 comments

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+  //Only change code belowe this line
+  /*console.log("Hello World!");*/
+  //<h1>Hello World!</h1>
+  //This is single line comment
+  /*This is multi-line comment
+  This is multi-line comment
+  This is multi-line comment
+  This is multi-line comment*/
+  //Only change code above this line
+}
+
+main();
+
+module.exports = main;
