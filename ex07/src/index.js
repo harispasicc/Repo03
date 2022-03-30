62a862af09c43ec27f983031ecc942abfe503a6e
Haris Pasic <harispasic@ymail.com>
2022-03-30 22:17:21 +0200

    Using camelCase

---
+++ ex07/src/index.js
@@ -0,0 +1,22 @@
+function main() {
+  //Only change code belowe this line
+  // Variable declaratoins
+  var studlyCapVar;
+  var properCamelCase;
+  var titleCaseOver;
+  // Variable assignments
+  studlyCapVar = 10;
+  properCamelCase = "A String";
+  titleCaseOver = 9000;
+  console.log(titleCaseOver);
+  //Only change code above this line
+  return {
+    studlyCapVar,
+    properCamelCase,
+    titleCaseOver,
+  };
+}
+
+main();
+
+module.exports = main;
